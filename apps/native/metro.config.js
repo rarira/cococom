4cd34ccbc31f04742162ae399407034eb5e010fa
Inseong Park <rariradev@gmail.com>
2024-06-26 01:15:41 +0900

    COC-1 Add: sentry and fix monorepo build

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,7 +1,46 @@
+// https://github.com/byCedric/expo-monorepo-example/blob/main/apps/mobile/metro.config.js
+
 // This replaces `const { getDefaultConfig } = require('expo/metro-config');`
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+const path = require("path");
+const { FileStore } = require("metro-cache");
 
 // This replaces `const config = getDefaultConfig(__dirname);`
-const config = getSentryExpoConfig(__dirname);
+
+// Find the project and workspace directories
+const projectRoot = __dirname;
+// This can be replaced with `find-yarn-workspace-root`
+const monorepoRoot = path.resolve(projectRoot, "../..");
+
+const config = getSentryExpoConfig(projectRoot);
+
+const monorepoPackages = {
+  //   "@acme/api": path.resolve(monorepoRoot, "packages/api"),
+  //   "@acme/components": path.resolve(monorepoRoot, "packages/components"),
+};
+
+// 1. Watch the local app folder, and only the shared packages (limiting the scope and speeding it up)
+// Note how we change this from `monorepoRoot` to `projectRoot`. This is part of the optimization!
+config.watchFolders = [projectRoot, ...Object.values(monorepoPackages)];
+
+// Add the monorepo workspaces as `extraNodeModules` to Metro.
+// If your monorepo tooling creates workspace symlinks in the `node_modules` folder,
+// you can either add symlink support to Metro or set the `extraNodeModules` to avoid the symlinks.
+// See: https://metrobundler.dev/docs/configuration/#extranodemodules
+config.resolver.extraNodeModules = monorepoPackages;
+config.resolver.disableHierarchicalLookup = true;
+
+// 2. Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(monorepoRoot, "node_modules"),
+];
+
+// Use turborepo to restore the cache when possible
+config.cacheStores = [
+  new FileStore({
+    root: path.join(projectRoot, "node_modules", ".cache", "metro"),
+  }),
+];
 
 module.exports = config;
