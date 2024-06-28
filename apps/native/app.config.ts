ea6d9e1a5689d22d112d49ce0a1c4f2fa16fef2d
Inseong Park <rariradev@gmail.com>
2024-06-28 14:43:15 +0900

    COC-1 Remove: sentry_auth_token

--- apps/native/app.config.ts
+++ apps/native/app.config.ts
@@ -12,7 +12,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       {
         organization: process.env.SENTRY_ORG,
         project: process.env.SENTRY_PROJECT,
-        authToken: process.env.SENTRY_AUTH_TOKEN,
       },
     ],
     [
