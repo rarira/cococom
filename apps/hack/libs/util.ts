2965cfa3c662caa31894bd7c42a30db92e27c375
Inseong Park <rariradev@gmail.com>
2024-06-30 00:54:11 +0900

    COC-2 Update: eslint

--- apps/hack/libs/util.ts
+++ apps/hack/libs/util.ts
@@ -2,6 +2,11 @@ import fs from 'fs';
 import dotenv from 'dotenv';
 import path from 'path';
 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
 export function writeJsonFile(filePath, data) {
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 }
