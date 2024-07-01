5620b1e39774218da87d928057b5ab40a38ccb01
Inseong Park <rariradev@gmail.com>
2024-07-01 22:08:49 +0900

    COC-1 Update: hack code

--- apps/hack/libs/supabase.ts
+++ apps/hack/libs/supabase.ts
@@ -8,4 +8,5 @@ loadEnv();
 // eslint-disable-next-line turbo/no-undeclared-env-vars
 const { SUPABASE_URL, SUPABASE_ANON_KEY } = process.env;
 
+
 export const supabase = new Supabase(SUPABASE_URL!, SUPABASE_ANON_KEY!);
