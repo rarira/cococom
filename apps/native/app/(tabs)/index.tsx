b7784697d6cbe7e3c21c015fc51a7a44b0d85b34
Inseong Park <rariradev@gmail.com>
2024-07-04 12:06:41 +0900

    COC-3 Setup: react-native-unistyles

--- apps/native/app/(tabs)/index.tsx
+++ apps/native/app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import analytics from '@react-native-firebase/analytics';
 import { Image } from 'expo-image';
-import { Button, StyleSheet, Platform } from 'react-native';
+import { Button } from 'react-native';
+import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -8,6 +9,8 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  const { styles } = useStyles(stylesheet);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -33,38 +36,11 @@ export default function HomeScreen() {
         />
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
     </ParallaxScrollView>
   );
 }
 
-const styles = StyleSheet.create({
+const stylesheet = createStyleSheet(theme => ({
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -81,4 +57,4 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
-});
+}));
