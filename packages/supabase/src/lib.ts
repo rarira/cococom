fbda2c07bbb48e2cb6b090bbeab0bc525fe009ba
Inseong Park <rariradev@gmail.com>
2024-07-06 23:27:29 +0900

    COC-3 Add: discount_rate view

--- packages/supabase/src/lib.ts
+++ packages/supabase/src/lib.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { SupabaseClient, createClient } from '@supabase/supabase-js';
 
-import { Database } from './types.js';
+import { Database } from './merged-types';
 
 // import { loadEnv } from './util.js';
 
@@ -71,7 +71,8 @@ export class Supabase {
 
     const { data, error } = await this.supabaseClient
       .from('discounts')
-      .select(`*, items(*)`)
+      // eslint-disable-next-line prettier/prettier
+      .select(`*,items(*, categories(*), discount_rate_view(*))`)
       .filter('startDate', 'lte', currentTimestamp)
       .filter('endDate', 'gte', currentTimestamp);
 
