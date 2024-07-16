44dc7ccf7c3f977e47054857da7cc19690bb5f11
Inseong Park <rariradev@gmail.com>
2024-07-17 01:19:59 +0900

    COC-6 Add: zustand and UIState

---
+++ apps/native/components/ui/view/modal-background/index.tsx
@@ -0,0 +1,23 @@
+import { View } from 'react-native';
+import { createStyleSheet, useStyles } from 'react-native-unistyles';
+
+interface ModalBackgroundViewProps {}
+
+function ModalBackgroundView({}: ModalBackgroundViewProps) {
+  const { styles } = useStyles(stylesheet);
+
+  return <View style={styles.container} />;
+}
+
+const stylesheet = createStyleSheet(theme => ({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+  },
+}));
+
+export default ModalBackgroundView;
