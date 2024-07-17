f5a0c70b208b6c506405447c2e6205307deafcd0
Inseong Park <rariradev@gmail.com>
2024-07-17 22:54:14 +0900

    COC-6 Add: NeedAuthDialog and navigation flow

--- apps/native/components/custom/button/list-item-wishlist-icon/index.tsx
+++ apps/native/components/custom/button/list-item-wishlist-icon/index.tsx
@@ -1,9 +1,10 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 import { ListItemCardProps } from '@/components/custom/card/list-item';
 import IconButton from '@/components/ui/button/icon';
 import { PortalHostNames } from '@/constants';
+import useSession from '@/hooks/useSession';
 
 import NeedAuthDialog from '../../dialog/need-auth';
 
@@ -12,6 +13,18 @@ interface ListItemWishlistIconButtonProps extends Pick<ListItemCardProps, 'disco
 function ListItemWishlistIconButton({ discount }: ListItemWishlistIconButtonProps) {
   const { styles, theme } = useStyles(stylesheet);
   const [needAuthDialogVisible, setNeedAuthDialogVisible] = useState(false);
+  const session = useSession();
+
+  const idToBeWishlistedRef = useRef<number | null>(null);
+
+  useLayoutEffect(() => {
+    if (session && needAuthDialogVisible) {
+      setNeedAuthDialogVisible(false);
+      if (idToBeWishlistedRef.current) {
+        console.log('idToBeWishlistedRef.current is', idToBeWishlistedRef.current);
+      }
+    }
+  }, [needAuthDialogVisible, session]);
 
   const iconProps = useMemo(() => {
     const isWishlistedByUser = !!discount.userWishlistCount;
@@ -22,8 +35,12 @@ function ListItemWishlistIconButton({ discount }: ListItemWishlistIconButtonProp
   }, [discount.userWishlistCount, theme]);
 
   const handlePress = useCallback(() => {
-    setNeedAuthDialogVisible(true);
-  }, []);
+    if (!session) {
+      idToBeWishlistedRef.current = discount.items.id;
+      setNeedAuthDialogVisible(true);
+      return;
+    }
+  }, [discount.items.id, session]);
 
   return (
     <>
--- apps/native/components/custom/dialog/need-auth/index.tsx
+++ apps/native/components/custom/dialog/need-auth/index.tsx
@@ -1,7 +1,10 @@
-import { Dispatch, SetStateAction } from 'react';
+import { router } from 'expo-router';
+import { Dispatch, SetStateAction, useCallback } from 'react';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
+import Button from '@/components/ui/button';
 import Dialog from '@/components/ui/dialog';
+import Text from '@/components/ui/text';
 import { PortalHostNames } from '@/constants';
 
 interface NeedAuthDialogProps {
@@ -13,58 +16,36 @@ interface NeedAuthDialogProps {
 function NeedAuthDialog(props: NeedAuthDialogProps) {
   const { styles } = useStyles(stylesheet);
 
+  const renderButtons = useCallback(() => {
+    return (
+      <Button onPress={() => router.push('/auth/signin')} style={styles.loginButton}>
+        <Text style={styles.loginText}>로그인</Text>
+      </Button>
+    );
+  }, [styles.loginButton, styles.loginText]);
+
   return (
     <Dialog
       {...props}
       title="로그인 필요"
       body="관심 상품 등록을 하시면 편리하게 쇼핑하실 수 있습니다. 로그인이 필요합니다"
-      // showCloseButton
-      // backdropDismiss
+      renderButtons={renderButtons}
+      showCloseButton
+      backdropDismiss
     />
   );
 }
 
 const stylesheet = createStyleSheet(theme => ({
-  modalView: {
-    flex: 1,
-    alignContent: 'center',
-    justifyContent: 'center',
-  },
-  alert: {
+  loginButton: {
+    backgroundColor: theme.colors.link,
     width: '100%',
-    maxWidth: 300,
-    margin: 48,
-    elevation: 24,
-    borderRadius: 2,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  alertTitle: {
-    margin: 24,
+  loginText: {
+    color: theme.colors.tint3,
     fontWeight: 'bold',
-    fontSize: 24,
-    color: '#000',
-  },
-  alertMessage: {
-    marginLeft: 24,
-    marginRight: 24,
-    marginBottom: 24,
-    fontSize: 16,
-    color: '#000',
-  },
-  alertButtonGroup: {
-    marginTop: 0,
-    marginRight: 0,
-    marginBottom: 8,
-    marginLeft: 24,
-    padding: 10,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-  },
-  alertButton: {
-    marginTop: 12,
-    marginRight: 8,
-    width: 100,
   },
 }));
 
