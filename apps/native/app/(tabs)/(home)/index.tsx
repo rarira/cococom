23811ba37ebd47320ec0be7b54c3e52578fd3fe8
Inseong Park <rariradev@gmail.com>
2024-07-20 01:20:44 +0900

    COC-8 Add: useHideTabBar

--- apps/native/app/(tabs)/(home)/index.tsx
+++ apps/native/app/(tabs)/(home)/index.tsx
@@ -1,33 +1,17 @@
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
-import { useQuery } from '@tanstack/react-query';
 import { View } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
-import DiscountList from '@/components/custom/list/discount';
-import { queryKeys } from '@/libs/react-query';
-import { supabase } from '@/libs/supabase';
-
-function fetchCurrentDiscountsByCategorySector() {
-  const currentTimestamp = new Date().toISOString().split('T')[0];
-
-  console.log({ currentTimestamp });
-  return supabase.fetchCurrentDiscountsByCategorySector(currentTimestamp);
-}
+import CategorySectorList from '@/components/custom/list/category-sector';
 
 export default function HomeScreen() {
   const { styles } = useStyles(stylesheet);
 
   const tabBarHeight = useBottomTabBarHeight();
 
-  const { data, error, isLoading } = useQuery({
-    queryKey: queryKeys.discounts.currentListByCategorySector(),
-    queryFn: () => fetchCurrentDiscountsByCategorySector(),
-  });
-
-  console.log('current data', { data, error });
   return (
     <View style={styles.container(tabBarHeight)}>
-      <DiscountList categorySector="디지털/가전" />
+      <CategorySectorList />
     </View>
   );
 }
