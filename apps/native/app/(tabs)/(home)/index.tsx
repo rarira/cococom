0b8ac909479e2fe7d9c2ba4535a097233970b1b7
Inseong Park <rariradev@gmail.com>
2024-07-19 19:59:31 +0900

    COC-8 Add: (home) nested router

---
+++ apps/native/app/(tabs)/(home)/index.tsx
@@ -0,0 +1,25 @@
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { View } from 'react-native';
+import { createStyleSheet, useStyles } from 'react-native-unistyles';
+
+import DiscountList from '@/components/custom/list/discount';
+
+export default function HomeScreen() {
+  const { styles } = useStyles(stylesheet);
+
+  const tabBarHeight = useBottomTabBarHeight();
+
+  return (
+    <View style={styles.container(tabBarHeight)}>
+      <DiscountList categorySector="디지털/가전" />
+    </View>
+  );
+}
+
+const stylesheet = createStyleSheet(theme => ({
+  container: (tabBarHeight: number) => ({
+    flex: 1,
+    backgroundColor: theme.colors.background,
+    paddingBottom: tabBarHeight + theme.spacing.lg,
+  }),
+}));
