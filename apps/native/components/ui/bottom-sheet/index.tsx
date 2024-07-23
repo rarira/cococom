937d53c17c16e06d7ca85761f376e0457b42e220
Inseong Park <rariradev@gmail.com>
2024-07-23 10:14:23 +0900

    COC-8 Update: bottomsheet

--- apps/native/components/ui/bottom-sheet/index.tsx
+++ apps/native/components/ui/bottom-sheet/index.tsx
@@ -1,17 +1,32 @@
-import { BottomSheetModal, BottomSheetModalProps, BottomSheetView } from '@gorhom/bottom-sheet';
+import {
+  BottomSheetBackdrop,
+  BottomSheetBackdropProps,
+  BottomSheetHandleProps,
+  BottomSheetModal,
+  BottomSheetModalProps,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet';
 import { BottomSheetViewProps } from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheetView/types';
 import { useHeaderHeight } from '@react-navigation/elements';
-import { forwardRef, memo, useMemo } from 'react';
+import { forwardRef, memo, useCallback, useMemo } from 'react';
+import { View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
+import ModalCloseButton from '@/components/custom/button/modal-close';
+import Text from '@/components/ui/text';
+
 interface BottomSheetProps
   extends Omit<BottomSheetModalProps, 'children'>,
-    Pick<BottomSheetViewProps, 'children'> {}
+    Pick<BottomSheetViewProps, 'children'> {
+  title?: string;
+  backdropDismiss?: boolean;
+  showCloseButton?: boolean;
+}
 
 export default memo(
   forwardRef<BottomSheetModal, BottomSheetProps>(function BottomSheet(
-    { children, ...restProps }: BottomSheetProps,
+    { title, children, backdropDismiss, showCloseButton = true, ...restProps }: BottomSheetProps,
     ref,
   ) {
     const { styles } = useStyles(stylesheet);
@@ -22,16 +37,38 @@ export default memo(
 
     const snapPoints = useMemo(() => ['25%'], []);
 
+    const backdropComponent = useCallback(
+      (props: BottomSheetBackdropProps) => <BottomSheetBackdrop {...props} />,
+      [],
+    );
+
+    const handleComponent = useCallback(
+      (props: BottomSheetHandleProps) => (
+        <>
+          {/* <BottomSheetHandle {...props} />
+           */}
+          <View style={styles.titleContainer}>
+            {/* <BottomSheetHandle {...props} style={styles.handleContainer} /> */}
+            <Text style={styles.title}>{title}</Text>
+            <ModalCloseButton onPress={() => ref.current?.dismiss()} show={showCloseButton} />
+          </View>
+        </>
+      ),
+      [ref, showCloseButton, styles.title, styles.titleContainer, title],
+    );
+
     return (
       <BottomSheetModal
         ref={ref}
-        index={1}
         snapPoints={snapPoints}
         enableDismissOnClose
+        enablePanDownToClose
         backgroundStyle={styles.background}
         enableDynamicSizing
         topInset={headerHeight}
-        handleIndicatorStyle={styles.handleIndicator}
+        backdropComponent={backdropComponent}
+        // handleIndicatorStyle={styles.handleIndicator}
+        handleComponent={handleComponent}
         {...restProps}
       >
         <BottomSheetView style={styles.container(bottomInset)}>{children}</BottomSheetView>
@@ -42,7 +79,8 @@ export default memo(
 
 const stylesheet = createStyleSheet(theme => ({
   container: (bottomInset: number) => ({
-    padding: theme.spacing.xl,
+    flexDirection: 'column',
+    paddingHorizontal: theme.spacing.xl,
     justifyContent: 'center',
     backgroundColor: theme.colors.modalBackground,
     paddingBottom: bottomInset,
@@ -50,7 +88,21 @@ const stylesheet = createStyleSheet(theme => ({
   background: {
     backgroundColor: theme.colors.modalBackground,
   },
-  handleIndicator: {
-    backgroundColor: theme.colors.tint,
+  // handleContainer: {
+  //   padding: 0,
+  //   marginBottom: theme.spacing.md,
+  // },
+  // handleIndicator: {
+  //   backgroundColor: theme.colors.tint,
+  // },
+  titleContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing.lg,
+  },
+  title: {
+    fontSize: theme.fontSize.md,
+    fontWeight: 'bold',
   },
 }));
