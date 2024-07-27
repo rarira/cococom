ca22ccd53a0dcfa1e07fc1a1e20b2be73834f438
Inseong Park <rariradev@gmail.com>
2024-07-27 22:07:44 +0900

    COC-10 Refactor TextInput UI component

--- apps/native/components/ui/text-input/index.tsx
+++ apps/native/components/ui/text-input/index.tsx
@@ -8,55 +8,86 @@ import {
 } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
-import Text, { TextProps } from '@/components/ui/text';
+import Icon, { IconProps } from '@/components/ui/icon';
 import Util from '@/libs/util';
 
-interface TextInputFieldProps extends RNTextInputProps {
-  outlined?: boolean;
+interface TextInputWrapperFields extends ViewProps {
+  variants?: 'outlined' | 'underlined' | 'rounded';
 }
 
-function TextInputWrapper({ style, ...restProps }: ViewProps) {
-  const { styles } = useStyles(stylesheet);
-
-  return <View style={[styles.wrapperContainer, style]} {...restProps} />;
-}
-
-function TextInputField({ style, outlined = true, ...restProps }: TextInputFieldProps) {
+function TextInputWrapper({ style, variants = 'outlined', ...restProps }: TextInputWrapperFields) {
   const { styles } = useStyles(stylesheet);
 
   return (
-    <RNTextInput style={[styles.input, [outlined ? styles.outlined : {}], style]} {...restProps} />
+    <View
+      style={[styles.wrapperContainer, [variants ? styles[variants] : {}], style]}
+      {...restProps}
+    />
   );
 }
 
-function TextInputLabel({ style, ...restProps }: TextProps) {
-  return <Text style={style} {...restProps} />;
+function TextInputField({ style, ...restProps }: RNTextInputProps) {
+  const { styles } = useStyles(stylesheet);
+
+  return <RNTextInput style={[styles.field, style]} {...restProps} />;
 }
 
 function TextInputSlot({ style, ...restProps }: PressableProps) {
-  return <Pressable style={style} {...restProps} />;
+  const { styles } = useStyles(stylesheet);
+
+  return (
+    <Pressable
+      style={state => [styles.slot, typeof style === 'function' ? style(state) : style]}
+      {...restProps}
+    />
+  );
 }
 
-function TextInputIcon({ style, ...restProps }: TextProps) {
-  return <Text style={style} {...restProps} />;
+function TextInputIcon({ style, ...restProps }: IconProps) {
+  const { theme } = useStyles();
+  return <Icon style={style} size={theme.fontSize.lg} {...restProps} />;
 }
 
 const stylesheet = createStyleSheet(theme => ({
   wrapperContainer: {
-    flexDirection: 'column',
+    flexDirection: 'row',
+    overflow: 'hidden',
     justifyContent: 'flex-start',
-  },
-  input: {
-    padding: theme.spacing.md,
-    borderRadius: theme.borderRadius.md,
-
-    color: theme.colors.typography,
-    fontSize: theme.fontSize.md,
+    width: '100%',
+    paddingVertical: theme.spacing.md,
+    paddingHorizontal: theme.spacing.md,
+    marginVertical: theme.spacing.sm,
   },
   outlined: {
     borderWidth: 1,
     borderColor: Util.hexToRgba(theme.colors.typography, 0.6),
   },
+  underlined: {
+    borderWidth: 0,
+    borderBottomWidth: 1,
+    borderBottomColor: Util.hexToRgba(theme.colors.typography, 0.6),
+  },
+  rounded: {
+    borderRadius: 9999,
+    borderWidth: 1,
+    borderColor: Util.hexToRgba(theme.colors.typography, 0.6),
+  },
+  field: {
+    flex: 1,
+    color: theme.colors.typography,
+    fontSize: theme.fontSize.md,
+  },
+  slot: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 }));
 
-export default TextInputField;
+const TextInput = {
+  Root: TextInputWrapper,
+  Field: TextInputField,
+  Slot: TextInputSlot,
+  Icon: TextInputIcon,
+};
+
+export default TextInput;
