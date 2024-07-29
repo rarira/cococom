99e634adb4dc1722981fc14a7fdd6cb1c2d913ac
Inseong Park <rariradev@gmail.com>
2024-07-29 13:09:42 +0900

    COC-10: Add Checkbox Context

--- apps/native/components/ui/checkbox/index.tsx
+++ apps/native/components/ui/checkbox/index.tsx
@@ -1,14 +1,97 @@
-import { memo } from 'react';
-import { View, ViewProps } from 'react-native';
+import Text from '@/components/ui/text';
+import { createContext, memo, ReactNode, Reducer, useContext, useEffect, useReducer } from 'react';
+import { Pressable, PressableProps, View } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
-interface CheckboxWrapperProps extends ViewProps {}
+interface CheckboxProps extends PressableProps {
+  value: string;
+  onChange?: (value: boolean) => void;
+  defaultIsChecked?: boolean;
+  isChecked?: boolean;
+}
 
-const CheckboxWrapper = memo(function Checkbox({ style, ...restProps }: CheckboxWrapperProps) {
+type CheckboxContextType = {
+  value: string;
+  isChecked: boolean;
+};
+
+type CheckBoxDispatchContextType = (context: Partial<CheckboxContextType>) => void;
+
+const CheckboxContext = createContext<CheckboxContextType | null>(null);
+
+const CheckboxDispatchContext = createContext<CheckBoxDispatchContextType | null>(null);
+
+function CheckboxProvider({ children }: { children: ReactNode }) {
+  const [state, setState] = useReducer<Reducer<CheckboxContextType, Partial<CheckboxContextType>>>(
+    (state, action) => ({ ...state, action }),
+    {
+      value: '',
+      isChecked: false,
+    },
+  );
+
+  return (
+    <CheckboxContext.Provider value={state}>
+      <CheckboxDispatchContext.Provider value={setState}>
+        {children}
+      </CheckboxDispatchContext.Provider>
+    </CheckboxContext.Provider>
+  );
+}
+
+function useCheckbox() {
+  const context = useContext(CheckboxContext);
+  const setContext = useContext(CheckboxDispatchContext);
+  if (!context || !setContext) {
+    throw new Error('useCheckbox must be used within a CheckboxProvider');
+  }
+  return [context, setContext] as const;
+}
+
+const CheckboxPressable = memo(function CheckboxPressable({
+  style,
+  isChecked,
+  defaultIsChecked,
+  ...restProps
+}: CheckboxProps) {
+  const [checboxContext, setCheckboxContext] = useCheckbox();
   const { styles } = useStyles(stylesheet);
 
-  return <View style={[styles.wrapper, style]} {...restProps} />;
+  useEffect(() => {
+    if (isChecked !== undefined) {
+      setCheckboxContext({ isChecked });
+    } else if (defaultIsChecked !== undefined) {
+      setCheckboxContext({ isChecked: defaultIsChecked });
+    }
+  }, [ defaultIsChecked, isChecked, setCheckboxContext]);
+
+  return (
+    <Pressable
+      style={state => [styles.wrapper, typeof style === 'function' ? style(state) : style]}
+      {...restProps}
+    />
+  );
 });
+
+const CheckboxWrapper = memo(function CheckboxWrapper(props: CheckboxProps) {
+  return (
+    <CheckboxProvider>
+      <CheckboxPressable {...props} />
+    </CheckboxProvider>
+  );
+});
+
+const CheckboxTest = memo(function CheckboxTest() {
+  const [context, setContext] = useCheckbox();
+  console.log('context', context);
+
+  return (
+    <View>
+      <Text>Checkbox</Text>
+    </View>
+  );
+});
+
 
 const stylesheet = createStyleSheet(theme => ({
   wrapper: {
@@ -21,6 +104,7 @@ const stylesheet = createStyleSheet(theme => ({
 
 const Checkbox = {
   Root: CheckboxWrapper,
+  Test: CheckboxTest,
 };
 
 export default Checkbox;
