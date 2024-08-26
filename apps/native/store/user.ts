770221065eb89cfb064828e77fffc5ddf03d6329
Inseong Park <rariradev@gmail.com>
2024-08-27 00:17:59 +0900

    COC-11 Update: profile table

--- apps/native/store/user.ts
+++ apps/native/store/user.ts
@@ -1,16 +1,20 @@
-import { User } from '@cococom/supabase/types';
+import { Tables, User } from '@cococom/supabase/types';
 import { create } from 'zustand';
 
 interface UserState {
   user: User | null;
+  profile: Tables<'profiles'> | null;
   callbackAfterSignIn: ((user: User) => void) | null;
   setUser: (user: User | null) => void;
+  setProfile: (profile: Tables<'profiles'> | null) => void;
   setCallbackAfterSignIn: (callback: ((user: User) => void) | null) => void;
 }
 
 export const useUserStore = create<UserState>()(set => ({
   user: null,
+  profile: null,
   callbackAfterSignIn: null,
   setUser: user => set({ user }),
+  setProfile: profile => set({ profile }),
   setCallbackAfterSignIn: callbackAfterSignIn => set({ callbackAfterSignIn }),
 }));
