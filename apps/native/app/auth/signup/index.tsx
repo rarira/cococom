5469ac8c473d2052ba217b22621aba456fa33876
Inseong Park <rariradev@gmail.com>
2024-09-05 10:36:46 +0900

    COC-13 Add: 로그인 화면 헤더 버튼

--- apps/native/app/auth/signup/index.tsx
+++ apps/native/app/auth/signup/index.tsx
@@ -1,5 +1,7 @@
+import { router } from 'expo-router';
 import { View } from 'react-native';
 
 export default function SignUpScreen() {
+  console.log('signup screen', router.canGoBack());
   return <View />;
 }
