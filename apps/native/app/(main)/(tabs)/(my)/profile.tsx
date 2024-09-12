fc749a179118c3ea195afac6594f8e8640c6528e
Inseong Park <rariradev@gmail.com>
2024-09-12 11:44:48 +0900

    COC-13 Misc: chores

--- apps/native/app/(main)/(tabs)/(my)/profile.tsx
+++ apps/native/app/(main)/(tabs)/(my)/profile.tsx
@@ -1,12 +1,11 @@
 import { router } from 'expo-router';
 import { useLayoutEffect } from 'react';
-import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 import SignUpConfirmForm from '@/components/custom/form/signup/&confirm';
 import ScreenContainerView from '@/components/custom/view/container/screen';
 import { useUserStore } from '@/store/user';
+
 export default function ProfileScreen() {
-  const { styles } = useStyles(stylesheet);
   const user = useUserStore(state => state.user);
 
   useLayoutEffect(() => {
@@ -19,12 +18,3 @@ export default function ProfileScreen() {
     </ScreenContainerView>
   );
 }
-
-const stylesheet = createStyleSheet(theme => ({
-  container: (topInset: number) => ({
-    flex: 1,
-    paddingHorizontal: theme.screenHorizontalPadding,
-    backgroundColor: theme.colors.background,
-    paddingTop: theme.spacing.lg,
-  }),
-}));
