5248689185f6a8f30484aceb942e1e733052829b
Inseong Park <rariradev@gmail.com>
2024-09-19 14:02:54 +0900

    COC-13 Add: noti permission ui only

--- apps/native/components/custom/text/section/index.tsx
+++ apps/native/components/custom/text/section/index.tsx
@@ -3,25 +3,33 @@ import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 import Text, { TextProps } from '@/components/ui/text';
 
-interface SectionTextProps extends TextProps {}
+interface SectionTextProps extends TextProps {
+  isFirstSection?: boolean;
+}
 
-const SectionText = memo(function SectionText({ style, children, ...restProps }: SectionTextProps) {
+const SectionText = memo(function SectionText({
+  isFirstSection,
+  style,
+  children,
+  ...restProps
+}: SectionTextProps) {
   const { styles } = useStyles(stylesheet);
 
   return (
-    <Text style={[styles.container, style]} {...restProps}>
+    <Text style={[styles.container(isFirstSection), style]} {...restProps}>
       {children}
     </Text>
   );
 });
 
 const stylesheet = createStyleSheet(theme => ({
-  container: {
+  container: (isFirstSection?: boolean) => ({
     fontWeight: 'bold',
     color: `${theme.colors.typography}80`,
     fontSize: theme.fontSize.normal,
-    marginVertical: theme.spacing.md,
-  },
+    marginTop: isFirstSection ? 0 : theme.spacing.md,
+    paddingVertical: theme.spacing.md,
+  }),
 }));
 
 export default SectionText;
