311cf7a657692283d32b4961464d2d1bcf22da76
Inseong Park <rariradev@gmail.com>
2024-09-23 15:25:19 +0900

    COC-14 Add: getAllCategoryInfo of  online

---
+++ apps/hack/src/online.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+import { writeJsonFile } from '../libs/util.js';
+
+const SITEMAP_URL = 'https://www.costco.co.kr/CostcoKorea_Sitemap';
+
+type SubCategoryLink = {
+  fullLink: string;
+  category: string;
+};
+
+async function getAllCategoryInfo() {
+  const subCategoryLinks: SubCategoryLink[] = [];
+  try {
+    const response = await axios.get(SITEMAP_URL);
+
+    const $ = cheerio.load(response.data);
+
+    $('.sub_category .sub-list-title a').each((index, element) => {
+      const fullLink = $(element).attr('href');
+      if (!fullLink) {
+        return;
+      }
+      const category = fullLink.split('/').pop();
+      if (!category?.startsWith('cos')) {
+        return;
+      }
+      const object = {
+        fullLink,
+        category,
+      };
+      subCategoryLinks.push(object);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log('subCategoryLinks', subCategoryLinks.length);
+
+  await writeJsonFile('temp/subCategoryLinks.json', subCategoryLinks);
+}
+
+(async () => {
+  await getAllCategoryInfo();
+})();
