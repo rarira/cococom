1ce0599d3d1e950ab81ed4134cf7977aac3e3142
Inseong Park <rariradev@gmail.com>
2024-09-24 14:14:00 +0900

    COC-14 Add: getAllItems

--- apps/hack/src/online.ts
+++ apps/hack/src/online.ts
@@ -1,20 +1,17 @@
 // eslint-disable-next-line import/order
-import { loadEnv, writeJsonFile } from '../libs/util.js';
+import { loadEnv, readJsonFile, writeJsonFile } from '../libs/util.js';
 
 loadEnv();
 
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 
-const CATEGORY_EXCLUDE = ['cos_whsonly', 'cos_22'];
+import { OnlineProduct, OnlineSubCategoryLink, SearchApiResult } from '../libs/types.js';
 
-type SubCategoryLink = {
-  fullLink: string;
-  category: string;
-};
+const CATEGORY_EXCLUDE = ['cos_whsonly', 'cos_22', 'cos_17_all'];
 
 async function getAllCategoryInfo() {
-  const subCategoryLinks: SubCategoryLink[] = [];
+  const subCategoryLinks: OnlineSubCategoryLink[] = [];
   try {
     // eslint-disable-next-line turbo/no-undeclared-env-vars
     const response = await axios.get(process.env['3RD_API_SITEMAP_URL']!);
@@ -33,6 +30,7 @@ async function getAllCategoryInfo() {
       const object = {
         fullLink,
         category,
+        title: $(element).text(),
       };
       subCategoryLinks.push(object);
     });
@@ -45,6 +43,60 @@ async function getAllCategoryInfo() {
   await writeJsonFile('temp/subCategoryLinks.json', subCategoryLinks);
 }
 
+async function getAllItemsByCategory(category: string) {
+  //   console.log('getAllItemsByCategory', category);
+  // eslint-disable-next-line turbo/no-undeclared-env-vars
+  const apiUrl = `${process.env['3RD_API_URL']}pageSize=100&category=${category}`;
+
+  try {
+    const response = await axios.get<SearchApiResult>(apiUrl);
+    if (response.data.pagination.totalPages === 1) {
+      return {
+        totalProducts: response.data.pagination.totalResults,
+        products: response.data.products,
+      };
+    }
+
+    const products = [...response.data.products];
+    for (let i = 1; i < response.data.pagination.totalPages; i++) {
+      const paginationResponse = await axios.get<SearchApiResult>(`${apiUrl}&currentPage=${i}`);
+      products.push(...paginationResponse.data.products);
+    }
+
+    if (response.data.pagination.totalResults !== products.length) {
+      console.error(
+        'pagination mismatch',
+        category,
+        response.data.pagination.totalResults,
+        response.data.products.length,
+      );
+    }
+
+    return { totalProducts: response.data.pagination.totalResults, products };
+  } catch (error) {
+    console.error(error);
+    return { totalProducts: 0, products: [] };
+  }
+}
+
+async function getAllItems() {
+  let productsCount = 0;
+
+  const allProducts: OnlineProduct[] = [];
+
+  const subCategoryLinks = await readJsonFile('temp/subCategoryLinks.json');
+  for (const subCategoryLink of subCategoryLinks) {
+    const { totalProducts, products } = await getAllItemsByCategory(subCategoryLink.category);
+    allProducts.push(...products);
+    productsCount += totalProducts;
+  }
+
+  await writeJsonFile('temp/products.json', allProducts);
+
+  console.log('product  array count', allProducts.length, 'productsCount', productsCount);
+}
+
 (async () => {
   await getAllCategoryInfo();
+  //   await getAllItems();
 })();
