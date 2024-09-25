6246f4958d137ae93b858ab37b75ece9b544cadb
Inseong Park <rariradev@gmail.com>
2024-09-25 16:18:17 +0900

    COC-14 Update: getAllCategoryInfo

--- apps/hack/src/online.ts
+++ apps/hack/src/online.ts
@@ -6,10 +6,82 @@ loadEnv();
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 
-import { updateSubCategoryLinks } from '../libs/openapi.js';
 import { OnlineProduct, OnlineSubCategoryLink, SearchApiResult } from '../libs/types.js';
 
 const CATEGORY_EXCLUDE = ['cos_whsonly', 'cos_22'];
+const CATEGORY_TO_DEEP = ['cos_10.1', 'cos_10.4', 'cos_10.10'];
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function createSubCategoryLink($: cheerio.CheerioAPI, element: any) {
+  const fullLink = $(element).attr('href');
+
+  if (!fullLink) {
+    return;
+  }
+
+  const category = fullLink.split('/c/').pop();
+
+  if (
+    !category?.startsWith('cos') ||
+    CATEGORY_EXCLUDE.includes(category) ||
+    category.startsWith('cos_17') ||
+    category.startsWith('cos_18')
+  ) {
+    return;
+  }
+
+  if (CATEGORY_TO_DEEP.includes(category)) {
+    const subSubCategories = getSubSubCategories($, element);
+
+    if (subSubCategories) {
+      return subSubCategories;
+    }
+  }
+
+  return [
+    {
+      fullLink: !fullLink.startsWith('https') ? `https://www.cost.co.kr${fullLink}` : fullLink,
+      category,
+      title: $(element).text(),
+    },
+  ];
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getSubSubCategories($: cheerio.CheerioAPI, element: any) {
+  const parentElement = $(element).parent();
+
+  if (!parentElement) {
+    return;
+  }
+
+  const childUlList = parentElement.next('ul');
+  if (!childUlList) {
+    return;
+  }
+
+  const childLiList = childUlList.children('li');
+
+  if (!childLiList) {
+    return;
+  }
+
+  const objectList: OnlineSubCategoryLink[] = [];
+
+  childLiList!.each(function () {
+    const aElement = $(this).children('a');
+    if (!aElement) {
+      return;
+    }
+
+    const object = createSubCategoryLink($, aElement);
+    if (object) {
+      objectList.push(...object);
+    }
+  });
+
+  return objectList;
+}
 
 async function getAllCategoryInfo() {
   const subCategoryLinks: OnlineSubCategoryLink[] = [];
@@ -19,25 +91,11 @@ async function getAllCategoryInfo() {
 
     const $ = cheerio.load(response.data);
 
-    $('.sub_category .sub-list-title a').each((index, element) => {
-      const fullLink = $(element).attr('href');
-      if (!fullLink) {
-        return;
+    $('.sub_category .sub-list-title a').each((_index, element) => {
+      const object = createSubCategoryLink($, element);
+      if (object) {
+        subCategoryLinks.push(...object);
       }
-      const category = fullLink.split('/c/').pop();
-      if (
-        !category?.startsWith('cos') ||
-        CATEGORY_EXCLUDE.includes(category) ||
-        category.startsWith('cos_17')
-      ) {
-        return;
-      }
-      const object = {
-        fullLink,
-        category,
-        title: $(element).text(),
-      };
-      subCategoryLinks.push(object);
     });
   } catch (error) {
     console.error(error);
@@ -45,11 +103,10 @@ async function getAllCategoryInfo() {
 
   console.log('subCategoryLinks', subCategoryLinks.length);
 
-  await writeJsonFile('temp/subCategoryLinks.json', subCategoryLinks);
+  await writeJsonFile('data/online_subCategoryLinks.json', subCategoryLinks);
 }
 
 async function getAllItemsByCategory(category: string) {
-  //   console.log('getAllItemsByCategory', category);
   // eslint-disable-next-line turbo/no-undeclared-env-vars
   const apiUrl = `${process.env['3RD_API_URL']}pageSize=100&category=${category}`;
 
@@ -104,5 +161,5 @@ async function getAllItems() {
 (async () => {
   await getAllCategoryInfo();
   //   await getAllItems();
-  await updateSubCategoryLinks();
+  // await updateSubCategoryLinks();
 })();
