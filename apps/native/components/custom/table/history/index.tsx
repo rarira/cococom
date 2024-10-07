faeb9eb34865fb6b6c31db663b230fec30f1a2d8
Inseong Park <rariradev@gmail.com>
2024-10-07 23:26:33 +0900

    COC-14 Update: HistoryBanner

---
+++ apps/native/components/custom/table/history/index.tsx
@@ -0,0 +1,81 @@
+import { Tables } from '@cococom/supabase/types';
+import { memo, useCallback } from 'react';
+import { View } from 'react-native';
+import { Cell, Row, Table, TableWrapper } from 'react-native-reanimated-table';
+import { createStyleSheet, useStyles } from 'react-native-unistyles';
+
+import Text from '@/components/core/text';
+import { formatDashedDate } from '@/libs/date';
+
+interface HistoryTableProps {
+  data: Omit<Tables<'histories'>, 'no_images' | 'no_price'>[];
+}
+
+const HistoryTable = memo(function HistoryTable({ data }: HistoryTableProps) {
+  const { styles } = useStyles(stylesheet);
+
+  const renderType = useCallback(
+    (isOnline: boolean) => {
+      return (
+        <View style={styles.column}>
+          <Text style={styles.cellText}>{isOnline ? '온라인' : '오프라인'}</Text>
+        </View>
+      );
+    },
+    [styles.cellText, styles.column],
+  );
+
+  return (
+    <Table style={styles.table}>
+      <Row
+        data={['구분', '신규 할인 수', '신규 싱품 수', '업데이트일']}
+        flexArr={[2, 2, 2, 3]}
+        style={styles.headerRow}
+        textStyle={styles.headerText}
+      />
+      {data.map(history => (
+        <TableWrapper key={history.id} style={styles.row}>
+          <Cell flex={2} data={renderType(history.is_online)} />
+          <Cell flex={2} data={history.added_discount_count} textStyle={styles.cellText} />
+          <Cell flex={2} data={history.new_item_count} textStyle={styles.cellText} />
+          <Cell flex={3} data={formatDashedDate(history.created_at)} textStyle={styles.cellText} />
+        </TableWrapper>
+      ))}
+    </Table>
+  );
+});
+
+const stylesheet = createStyleSheet(theme => ({
+  table: { marginTop: theme.spacing.sm },
+  headerRow: {
+    flexDirection: 'row',
+    paddingVertical: theme.spacing.md,
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colors.background,
+    gap: theme.spacing.md,
+  },
+  headerText: {
+    fontWeight: 'semibold',
+    fontSize: theme.fontSize.normal,
+    color: theme.colors.background,
+    textAlign: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    paddingVertical: theme.spacing.sm,
+    gap: theme.spacing.md,
+  },
+  column: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cellText: {
+    color: theme.colors.background,
+    fontSize: theme.fontSize.sm,
+    lineHeight: theme.fontSize.sm,
+    textAlign: 'center',
+  },
+}));
+
+export default HistoryTable;
---
+++ packages/supabase/sqls/get_lastest_histories.sql
@@ -0,0 +1,30 @@
+create or replace function get_latest_histories()
+returns table (
+    id bigint,
+    created_at timestamp with time zone,
+    new_item_count integer,
+    added_discount_count integer,
+    is_online boolean
+) as $$
+begin
+    return query
+    (
+        -- 최신의 is_online = true 레코드
+        select h.id, h.created_at, h.new_item_count, h.added_discount_count, h.is_online
+        from public.histories h
+        where h.is_online = true
+        order by h.created_at desc
+        limit 1
+    );
+    
+    return query
+    (
+        -- 최신의 is_online = false 레코드
+        select h.id, h.created_at, h.new_item_count, h.added_discount_count, h.is_online
+        from public.histories h
+        where h.is_online = false
+        order by h.created_at desc
+        limit 1
+    );
+end;
+$$ language plpgsql;
