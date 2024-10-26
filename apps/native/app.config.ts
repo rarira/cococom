6ae67066425eca62759214107a1fe60aa14c7c36
Inseong Park <rariradev@gmail.com>
2024-10-27 00:00:25 +0900

    sdk-52 Update:  app.config.ts to             "newArchEnabled": true

--- apps/native/app.config.ts
+++ apps/native/app.config.ts
@@ -30,8 +30,10 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       {
         ios: {
           useFrameworks: 'static',
+          newArchEnabled: true,
         },
         android: {
+          newArchEnabled: true,
           extraMavenRepos: ['https://devrepo.kakao.com/nexus/content/groups/public/'],
         },
       },
