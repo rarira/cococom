467cc970f2ff383a8ecd939f25eed60079609e49
Inseong Park <rariradev@gmail.com>
2024-10-30 00:17:24 +0900

    remove: react-native-shadow-2

--- apps/native/components/custom/card/list-item/wishlist/index.tsx
+++ apps/native/components/custom/card/list-item/wishlist/index.tsx
@@ -2,14 +2,13 @@ import { QueryKey, useMutation, useQueryClient } from '@tanstack/react-query';
 import { Href, Link } from 'expo-router';
 import React, { memo, useCallback } from 'react';
 import { Pressable, StyleProp, View, ViewStyle } from 'react-native';
-import { Shadow } from 'react-native-shadow-2';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 import Card from '@/components/core/card';
 import ProductCardThumbnailImage from '@/components/custom/image/list-item-card-thumbnail';
 import WishlistItemCardDetailView from '@/components/custom/view/list-item-card/wishlist/&detail';
 import { WishlistToRender } from '@/hooks/wishlist/useWishlists';
-import { shadowPresets } from '@/libs/shadow';
+import { ShadowPresets } from '@/libs/shadow';
 import { supabase } from '@/libs/supabase';
 import { handleMutateOfWishlist } from '@/libs/wishlist';
 
@@ -60,39 +59,35 @@ const WishlistItemCard = memo(function WishlistItemCard({
       onLongPress={handleLongPress}
     >
       <Pressable>
-        <Shadow
-          {...shadowPresets.card(theme)}
-          {...(item.isOnSaleNow && { startColor: `${theme.colors.tint}44` })}
-          stretch
-          style={styles.container}
-        >
-          <Card style={[styles.cardContainer(item.isOnSaleNow, item.is_online), containerStyle]}>
-            <View style={styles.itemContainer}>
-              <ProductCardThumbnailImage
-                product={item}
-                width={80}
-                height={80}
-                style={styles.thumbnail}
-                isOnline={item.is_online}
-                small
-              />
-              <WishlistItemCardDetailView item={item} />
-            </View>
-          </Card>
-        </Shadow>
+        <Card style={[styles.cardContainer(item.isOnSaleNow, item.is_online), containerStyle]}>
+          <View style={styles.itemContainer}>
+            <ProductCardThumbnailImage
+              product={item}
+              width={80}
+              height={80}
+              style={styles.thumbnail}
+              isOnline={item.is_online}
+              small
+            />
+            <WishlistItemCardDetailView item={item} />
+          </View>
+        </Card>
       </Pressable>
     </Link>
   );
 });
 
 const stylesheet = createStyleSheet(theme => ({
-  container: {
-    backgroundColor: theme.colors.cardBackground,
-  },
   cardContainer: (onSale: boolean, isOnline: boolean) => ({
     borderRadius: theme.borderRadius.lg,
-    ...(onSale && { borderColor: `${theme.colors.tint}77`, borderWidth: 1 }),
     backgroundColor: isOnline ? `${theme.colors.tint3}11` : theme.colors.cardBackground,
+    ...ShadowPresets.card(theme),
+    ...(onSale && {
+      borderColor: `${theme.colors.tint}77`,
+      borderWidth: 1,
+      shadowColor: `${theme.colors.tint}44`,
+      shadowOpacity: 1,
+    }),
   }),
   itemContainer: {
     flex: 1,
