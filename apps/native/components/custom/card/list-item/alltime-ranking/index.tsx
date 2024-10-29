31d5a4c63823d0dbb6c1597704f6674d4f41e9b9
Inseong Park <rariradev@gmail.com>
2024-10-30 00:17:24 +0900

    remove: react-native-shadow-2

--- apps/native/components/custom/card/list-item/alltime-ranking/index.tsx
+++ apps/native/components/custom/card/list-item/alltime-ranking/index.tsx
@@ -2,13 +2,12 @@ import { AlltimeRankingResultItem } from '@cococom/supabase/types';
 import { Href, Link } from 'expo-router';
 import React, { memo } from 'react';
 import { Pressable, StyleProp, View, ViewStyle } from 'react-native';
-import { Shadow } from 'react-native-shadow-2';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 import Card from '@/components/core/card';
 import ProductCardThumbnailImage from '@/components/custom/image/list-item-card-thumbnail';
 import AlltimeRankingListItemCardDetailView from '@/components/custom/view/list-item-card/alltime-ranking/&detail';
-import { shadowPresets } from '@/libs/shadow';
+import { ShadowPresets } from '@/libs/shadow';
 
 export interface AlltimeRankingListItemCardProps {
   item: AlltimeRankingResultItem;
@@ -19,38 +18,39 @@ const AlltimeRankingListItemCard = memo(function AlltimeRankingListItemCard({
   item,
   containerStyle,
 }: AlltimeRankingListItemCardProps) {
-  const { styles, theme } = useStyles(stylesheet);
+  const { styles } = useStyles(stylesheet);
 
   return (
     <Link href={`/(ranking)/item?itemId=${item.id}` as Href<string>} asChild>
       <Pressable>
-        <Shadow
-          {...shadowPresets.card(theme)}
-          {...(item.isOnSaleNow && { startColor: `${theme.colors.tint}44` })}
-          stretch
-        >
-          <Card style={[styles.cardContainer(item.isOnSaleNow), containerStyle]}>
-            <View style={styles.itemContainer}>
-              <ProductCardThumbnailImage
-                product={item}
-                width={120}
-                height={120}
-                style={styles.thumbnail}
-                isOnline={item.is_online}
-              />
-              <AlltimeRankingListItemCardDetailView item={item} />
-            </View>
-          </Card>
-        </Shadow>
+        <Card style={[styles.cardContainer(item.isOnSaleNow, item.is_online), containerStyle]}>
+          <View style={styles.itemContainer}>
+            <ProductCardThumbnailImage
+              product={item}
+              width={120}
+              height={120}
+              style={styles.thumbnail}
+              isOnline={item.is_online}
+            />
+            <AlltimeRankingListItemCardDetailView item={item} />
+          </View>
+        </Card>
       </Pressable>
     </Link>
   );
 });
 
 const stylesheet = createStyleSheet(theme => ({
-  cardContainer: (onSale: boolean) => ({
+  cardContainer: (onSale: boolean, isOnline: boolean) => ({
     borderRadius: theme.borderRadius.lg,
-    ...(onSale && { borderColor: `${theme.colors.tint}77`, borderWidth: 1 }),
+    ...ShadowPresets.card(theme),
+    backgroundColor: isOnline ? `${theme.colors.tint3}11` : theme.colors.cardBackground,
+    ...(onSale && {
+      borderColor: `${theme.colors.tint}77`,
+      borderWidth: 1,
+      shadowColor: `${theme.colors.tint}44`,
+      shadowOpacity: 1,
+    }),
   }),
   itemContainer: {
     flex: 1,
