f18b6c30b63566646d89ca6de9b5433bdd1dad8a
Inseong Park <rariradev@gmail.com>
2024-11-06 21:49:27 +0900

    CDC-15 Add: useMyMemos

--- packages/supabase/src/lib/memos.ts
+++ packages/supabase/src/lib/memos.ts
@@ -1,6 +1,6 @@
 import { SupabaseClient } from '@supabase/supabase-js';
 
-import { Database, InsertMemo } from '../merged-types';
+import { Database, InsertMemo, JoinedMyMemos, SortOptionDirection } from '../merged-types';
 
 export class MemosTable {
   private supabaseClient: SupabaseClient<Database>;
@@ -54,5 +54,41 @@ export class MemosTable {
     }
 
     return data;
+  }
+
+  async fetchMyMemos({
+    userId,
+    page,
+    pageSize = 20,
+    orderBy = 'created_at',
+    orderDirection = 'DESC',
+  }: {
+    userId: string;
+    page: number;
+    pageSize?: number;
+    orderBy?: string;
+    orderDirection?: SortOptionDirection;
+  }) {
+    let promise = this.supabaseClient
+      .from('memos')
+      .select('id, created_at, updated_at, content, item:items (id, itemName, itemId)')
+      .eq('userId', userId);
+
+    if (orderBy?.startsWith('item.')) {
+      promise = promise.order(`item(${orderBy.split('.')[1]})`, {
+        ascending: orderDirection !== 'DESC',
+      });
+    } else {
+      promise = promise.order(orderBy, { ascending: orderDirection !== 'DESC' });
+    }
+
+    const { data, error } = await promise.range(page * pageSize, (page + 1) * pageSize - 1);
+
+    if (error) {
+      console.error(error);
+      throw error;
+    }
+
+    return data as unknown as JoinedMyMemos[];
   }
 }
