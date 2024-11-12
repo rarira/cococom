9961cb1bfe23dabfb24e9506d616d5538ce57905
Inseong Park <rariradev@gmail.com>
2024-11-12 13:20:55 +0900

    HACK: 20241112

--- apps/hack/libs/util.ts
+++ apps/hack/libs/util.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { readdir, stat, rm, unlink } from 'node:fs/promises';
+import { readdir, stat, rm, unlink, rmdir } from 'node:fs/promises';
 
 import dotenv from 'dotenv';
 
@@ -43,5 +43,63 @@ export async function clearFolder(folderPath: string) {
     console.log('Folder contents cleared successfully.');
   } catch (error) {
     console.error('Error clearing folder:', error);
+  }
+}
+
+export async function getMostRecentFile(folderPath: string, prefix: string) {
+  try {
+    // 파일 이름 패턴 설정 (예: jpgToWebp_YYYY-MM-DD.json)
+    const datePattern = new RegExp(`^${prefix}_(\\d{4}-\\d{2}-\\d{2})\\.json$`);
+
+    // 폴더 내 파일 목록을 읽어옵니다
+    const files = await readdir(folderPath);
+
+    // 형식이 맞는 파일들만 필터링
+    const datedFiles = files
+      .map(file => {
+        const match = file.match(datePattern);
+        if (match) {
+          return { file, date: match[1] ? new Date(match[1]) : new Date(0) };
+        }
+        return null;
+      })
+      .filter(Boolean); // 유효한 파일만 남기기
+
+    if (datedFiles.length === 0) {
+      console.log('No matching files found.');
+      return null;
+    }
+
+    // 최신 날짜의 파일 찾기
+    const mostRecentFile = datedFiles.reduce((latest, current) =>
+      current && latest && latest.date > current.date ? latest : current,
+    );
+
+    // 가장 최신 파일 경로
+    const mostRecentFilePath = path.join(folderPath, mostRecentFile!.file);
+
+    console.log(`Most recent file: ${mostRecentFile!.file}`);
+    return mostRecentFilePath;
+  } catch (err) {
+    console.error('Error reading folder:', err);
+  }
+}
+
+export async function deleteFolderIfEmpty(folderPath: string) {
+  try {
+    // 폴더 내 파일 목록을 읽어옵니다
+    const files = await readdir(folderPath);
+
+    // 파일이 없는 경우 폴더 삭제
+    if (files.length === 0) {
+      await rmdir(folderPath); // 폴더가 비어 있을 때만 삭제 가능
+      console.log(`Folder '${folderPath}' is empty and has been deleted.`);
+    } else {
+      console.log(
+        `Folder '${folderPath}' contains ${files.length} file(s) and will not be deleted.`,
+      );
+    }
+  } catch (err) {
+    console.error('Error checking folder:', err);
   }
 }
--- apps/hack/src/image_arrange.ts
+++ apps/hack/src/image_arrange.ts
@@ -1,10 +1,18 @@
 /* eslint-disable import/order */
 import { supabase } from '../libs/supabase.js';
-import { clearFolder, loadEnv, readJsonFile, writeJsonFile } from '../libs/util.js';
+import {
+  clearFolder,
+  getMostRecentFile,
+  loadEnv,
+  readJsonFile,
+  writeJsonFile,
+  deleteFolderIfEmpty,
+} from '../libs/util.js';
 
 loadEnv();
 
-import { access, copyFile, rm } from 'node:fs/promises';
+import { access, copyFile, unlink, mkdir } from 'node:fs/promises';
+import sharp from 'sharp';
 
 const date = new Date().toISOString().split('T')[0];
 
@@ -27,6 +35,28 @@ async function fetchAllItems() {
   }
 }
 
+function populateUniqueItemIds(item: ItemWithItemId, uniqueItemIds: UniqueItemId) {
+  const [itemId, isOnline] = item.itemId.split('_');
+
+  if (!itemId) return;
+
+  if (!uniqueItemIds[itemId]) {
+    uniqueItemIds[itemId] = { online: null, offline: null };
+  }
+
+  if (isOnline === 'online') {
+    if (uniqueItemIds[itemId].online !== null) {
+      console.error('already populated online itemId', item.id, item.itemId);
+    }
+    uniqueItemIds[itemId].online = item.id;
+  } else {
+    if (uniqueItemIds[itemId].offline !== null) {
+      console.error('already populated offline itemId', item.id, item.itemId);
+    }
+    uniqueItemIds[itemId].offline = item.id;
+  }
+}
+
 async function getUniqueItemIds() {
   const allItems = (await readJsonFile(
     `data/fetchAllItemsWithId_${date}.json`,
@@ -35,25 +65,7 @@ async function getUniqueItemIds() {
   const uniqueItemIds: UniqueItemId = {};
 
   allItems.forEach(item => {
-    const [itemId, isOnline] = item.itemId.split('_');
-
-    if (!itemId) return;
-
-    if (!uniqueItemIds[itemId]) {
-      uniqueItemIds[itemId] = { online: null, offline: null };
-    }
-
-    if (isOnline === 'online') {
-      if (uniqueItemIds[itemId].online !== null) {
-        console.error('already populated online itemId', item.id, item.itemId);
-      }
-      uniqueItemIds[itemId].online = item.id;
-    } else {
-      if (uniqueItemIds[itemId].offline !== null) {
-        console.error('already populated offline itemId', item.id, item.itemId);
-      }
-      uniqueItemIds[itemId].offline = item.id;
-    }
+    populateUniqueItemIds(item, uniqueItemIds);
   });
 
   await writeJsonFile(`data/getUniqueItemIds${date}.json`, uniqueItemIds);
@@ -91,14 +103,20 @@ async function verifyUniqueItemIds() {
   console.log('verifyUniqueItemIds', { count });
 }
 
-async function arrangeImageFiles() {
-  const uniqueItemIds = (await readJsonFile(
-    `data/getUniqueItemIds${date}.json`,
-  )) as unknown as UniqueItemId;
-
-  let noImagesCount = 0;
-
-  await clearFolder('downloads/final');
+async function searchImageAndCopy({
+  uniqueItemIds,
+  destinationPath,
+  noImagesCount,
+}: {
+  uniqueItemIds: UniqueItemId;
+  destinationPath: string;
+  noImagesCount?: number;
+}) {
+  try {
+    await access(destinationPath);
+  } catch {
+    await mkdir(destinationPath, { recursive: true });
+  }
 
   function getImagePathPromises(itemId: string) {
     const promises: Record<string, Promise<void>> = {};
@@ -131,6 +149,8 @@ async function arrangeImageFiles() {
   }
 
   for (const uniqueItemId in uniqueItemIds) {
+    if (uniqueItemIds[uniqueItemId]?.finalImageExt) continue;
+
     const promisesObject = getImagePathPromises(uniqueItemId);
 
     const paths = Object.keys(promisesObject);
@@ -143,7 +163,7 @@ async function arrangeImageFiles() {
       if (firstPathIndex === -1) {
         // console.error('no image found', uniqueItemId);
         uniqueItemIds[uniqueItemId]!.finalImageExt = null;
-        noImagesCount++;
+        typeof noImagesCount !== 'undefined' && noImagesCount++;
         continue;
       }
 
@@ -152,13 +172,25 @@ async function arrangeImageFiles() {
 
       await copyFile(
         getRealPath(uniqueItemId, firstPath)!,
-        `downloads/final/${uniqueItemId}.${extension}`,
+        `${destinationPath}${uniqueItemId}.${extension}`,
       );
       uniqueItemIds[uniqueItemId]!.finalImageExt = extension as 'webp' | 'jpg';
     } catch (error) {
       console.error(error);
     }
   }
+}
+
+async function arrangeImageFiles() {
+  const uniqueItemIds = (await readJsonFile(
+    `data/getUniqueItemIds${date}.json`,
+  )) as unknown as UniqueItemId;
+
+  const noImagesCount = 0;
+
+  await clearFolder('downloads/final');
+
+  await searchImageAndCopy({ uniqueItemIds, destinationPath: 'downloads/final/', noImagesCount });
 
   await writeJsonFile(`data/arrangeImageFiles_${date}.json`, uniqueItemIds);
 
@@ -168,9 +200,76 @@ async function arrangeImageFiles() {
   });
 }
 
+async function jpgToWebp() {
+  const uniqueItemIds = (await readJsonFile(
+    `data/arrangeImageFiles_${date}.json`,
+  )) as unknown as UniqueItemId;
+
+  const convertPromises: Promise<void>[] = [];
+
+  for (const uniqueItemId in uniqueItemIds) {
+    if (uniqueItemIds[uniqueItemId]?.finalImageExt === 'jpg') {
+      // convert to webp
+      convertPromises.push(
+        (async () => {
+          try {
+            await sharp(`downloads/final/${uniqueItemId}.jpg`)
+              .webp()
+              .toFile(`downloads/final/${uniqueItemId}.webp`);
+
+            await unlink(`downloads/final/${uniqueItemId}.jpg`);
+            uniqueItemIds[uniqueItemId]!.finalImageExt = 'webp';
+          } catch (error) {
+            console.error('convertJpgToWebp error', { error, uniqueItemId });
+          }
+        })(),
+      );
+    }
+  }
+
+  await Promise.allSettled(convertPromises);
+
+  await writeJsonFile(`data/jpgToWebp_${date}.json`, uniqueItemIds);
+}
+
+async function updateNewlyAddedImages() {
+  const mostRecentJpgToWebpFilePath = await getMostRecentFile('data', 'jpgToWebp');
+  console.log({ mostRecentJpgToWebpFilePath });
+
+  if (!mostRecentJpgToWebpFilePath) {
+    console.error('No jpgToWebp file found');
+    return;
+  }
+
+  const uniqueItemIds = (await readJsonFile(
+    mostRecentJpgToWebpFilePath,
+  )) as unknown as UniqueItemId;
+
+  const currentAllItems = (await readJsonFile(
+    `data/fetchAllItemsWithId_${date}.json`,
+  )) as unknown as ItemWithItemId[];
+
+  currentAllItems.forEach(item => {
+    const itemId = item.itemId.split('_')[0];
+
+    if (!itemId) return;
+    if (uniqueItemIds[itemId]) return;
+
+    populateUniqueItemIds(item, uniqueItemIds);
+  });
+
+  await searchImageAndCopy({ uniqueItemIds, destinationPath: `downloads/final/${date}/` });
+
+  await deleteFolderIfEmpty(`downloads/final/${date}/`);
+
+  await writeJsonFile(`data/jpgToWebp_${date}.json`, uniqueItemIds);
+}
+
 (async () => {
-  await fetchAllItems();
-  await getUniqueItemIds();
-  await verifyUniqueItemIds();
-  await arrangeImageFiles();
+  // await fetchAllItems();
+  // await getUniqueItemIds();
+  // await verifyUniqueItemIds();
+  // await arrangeImageFiles();
+  // await jpgToWebp();
+  await updateNewlyAddedImages();
 })();
