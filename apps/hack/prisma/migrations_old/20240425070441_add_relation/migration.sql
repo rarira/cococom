c736a96dbe2e3cfc11ccc8ed5dc0cae3344f89e0
Inseong Park <rariradev@gmail.com>
2024-11-20 21:12:22 +0900

    CDC-17 Remove: prisma related from apps/hack

--- apps/hack/prisma/migrations_old/20240425070441_add_relation/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "discounts" ADD CONSTRAINT "discounts_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "items"("itemId") ON DELETE RESTRICT ON UPDATE CASCADE;
--- apps/hack/prisma/migrations_old/20240426123633_relate_category/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "items" ADD CONSTRAINT "items_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
