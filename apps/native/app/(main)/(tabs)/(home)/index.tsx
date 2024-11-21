1e6f747c474f3a64b76ce64f0395b6743393bef6
Inseong Park <rariradev@gmail.com>
2024-11-21 23:29:19 +0900

    CDC-17 Test: code

--- apps/native/app/(main)/(tabs)/(home)/index.tsx
+++ apps/native/app/(main)/(tabs)/(home)/index.tsx
@@ -4,16 +4,22 @@ import { Button } from 'react-native';
 import CategorySectorList from '@/components/custom/list/category-sector';
 import ScreenContainerView from '@/components/custom/view/container/screen';
 import HistoryInfoBanner from '@/components/custom/view/history-banner';
+import { useErrorHandler } from '@/hooks/useErrorHandler';
 
 export default function HomeScreen() {
   const [totalDiscounts, setTotalDiscounts] = useState<number>(0);
+  const { reportToSentry } = useErrorHandler();
 
   return (
     <ScreenContainerView withBottomTabBar>
       <Button
         title="Press me"
         onPress={() => {
-          throw new Error('Hello, again, Sentry!');
+          try {
+            throw new Error('Hello, again, Sentry!');
+          } catch (error) {
+            reportToSentry(error as Error);
+          }
         }}
       />
 
