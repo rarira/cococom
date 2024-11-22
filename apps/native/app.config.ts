79b42ddd8f7d23cfc828b138ce3f18ea12598116
Inseong Park <rariradev@gmail.com>
2024-11-22 17:49:40 +0900

    Misc: chores

--- apps/native/app.config.ts
+++ apps/native/app.config.ts
@@ -27,72 +27,73 @@ function getSupabaseEnv(env: 'LOCAL' | 'PREVIEW' | 'PRODUCTION') {
 
 export default ({ config }: ConfigContext): ExpoConfig => {
   const supabaseEnv = getSupabaseEnv(process.env.supabaseEnv as 'LOCAL' | 'PREVIEW' | 'PRODUCTION');
-  console.log({supabaseEnv})
-  return ({
-  ...config,
-  name: 'cococom',
-  slug: 'cococom',
-  newArchEnabled: true,
-  plugins: [
-    'expo-router',
-    '@react-native-firebase/app',
-    '@react-native-firebase/crashlytics',
-    [
-      '@sentry/react-native/expo',
-      {
-        organization: process.env.SENTRY_ORG,
-        project: process.env.SENTRY_PROJECT,
-      },
-    ],
-    [
-      'expo-build-properties',
-      {
-        ios: {
-          useFrameworks: 'static',
+  console.log({ supabaseEnv });
+  return {
+    ...config,
+    name: 'cococom',
+    slug: 'cococom',
+    newArchEnabled: true,
+    plugins: [
+      'expo-router',
+      '@react-native-firebase/app',
+      '@react-native-firebase/crashlytics',
+      [
+        '@sentry/react-native/expo',
+        {
+          organization: process.env.SENTRY_ORG,
+          project: process.env.SENTRY_PROJECT,
         },
-        android: {
-          extraMavenRepos: ['https://devrepo.kakao.com/nexus/content/groups/public/'],
+      ],
+      [
+        'expo-build-properties',
+        {
+          ios: {
+            useFrameworks: 'static',
+          },
+          android: {
+            extraMavenRepos: ['https://devrepo.kakao.com/nexus/content/groups/public/'],
+          },
         },
-      },
+      ],
+      [
+        '@react-native-kakao/core',
+        {
+          nativeAppKey: process.env.KAKAO_TEST_NATIVE_APP_KEY,
+          android: {
+            authCodeHandlerActivity: true,
+          },
+          ios: {
+            handleKakaoOpenUrl: true,
+          },
+        },
+      ],
+      '@react-native-google-signin/google-signin',
+      'expo-apple-authentication',
+      './plugins',
+      'expo-font',
     ],
-    [
-      '@react-native-kakao/core',
-      {
+    ios: {
+      ...config.ios,
+      googleServicesFile:
+        process.env.GOOGLE_SERVICES_INFO ?? './assets/firebase/GoogleService-Info.plist',
+    },
+    android: {
+      ...config.android,
+      googleServicesFile:
+        process.env.GOOGLE_SERVICES_JSON ?? './assets/firebase/google-services.json',
+    },
+    extra: {
+      ...config.extra,
+      sentry: {
+        dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+      },
+      supabase: supabaseEnv,
+      kakao: {
         nativeAppKey: process.env.KAKAO_TEST_NATIVE_APP_KEY,
-        android: {
-          authCodeHandlerActivity: true,
-        },
-        ios: {
-          handleKakaoOpenUrl: true,
-        },
       },
-    ],
-    '@react-native-google-signin/google-signin',
-    'expo-apple-authentication',
-    './plugins',
-    'expo-font',
-  ],
-  ios: {
-    ...config.ios,
-    googleServicesFile:
-      process.env.GOOGLE_SERVICES_INFO ?? './assets/firebase/GoogleService-Info.plist',
-  },
-  android: {
-    ...config.android,
-    googleServicesFile:
-      process.env.GOOGLE_SERVICES_JSON ?? './assets/firebase/google-services.json',
-  },
-  extra: {
-    ...config.extra,
-    sentry: {
-      dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
     },
-    supabase: supabaseEnv,
-    kakao: {
-      nativeAppKey: process.env.KAKAO_TEST_NATIVE_APP_KEY,
+    updates: {
+      url: 'https://u.expo.dev/aad6f74a-0f9e-4ef0-9fb3-2a19791cb1ec',
     },
-  },
-  updates: {
-    url: 'https://u.expo.dev/aad6f74a-0f9e-4ef0-9fb3-2a19791cb1ec',
-  },
-}});
+  };
+};
