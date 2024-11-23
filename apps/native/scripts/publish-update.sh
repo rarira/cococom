6ac74024d2f37ed4278fbfd34749f8cb4ade013b
Inseong Park <rariradev@gmail.com>
2024-11-23 14:30:23 +0900

    CDC-17 Add: publish-update script

---
+++ apps/native/scripts/publish-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+environment=$1
+development_client=$(jq -r ".build.${environment}.developmentClient" eas.json)
+
+
+if [ "$development_client" = "true" ]; then
+  echo "$environment 대상으로는 업데이트를 할 수 없습니다."
+  exit 1
+fi
+
+supabase_env=$(jq -r ".build.${environment}.env.SUPABASE_ENV" eas.json)
+
+echo "$environment 대상으로 eas update를 시작합니다."
+SUPABASE_ENV="$supabase__env" eas update --auto --channel="$environment"
+
+echo "$environment 대상으로 sentry sourcemaps 업로드를 시작합니다."
+npx sentry-expo-upload-sourcemaps dist
